acb262206afcdf49b7a2d6e0b64734b7294085a3
Owen Valdescruz <42687533+ov37@users.noreply.github.com>
2021-01-16 12:56:29 -0500

    Update scene-one.js

--- scene-one.js
+++ scene-one.js
@@ -5,10 +5,29 @@ var SceneOne = new Phaser.Class({
     },
     init: function() {},
     preload: function() {
-        this.load.image("plane", "star.png");
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        //this.load.image('star', 'assets/star.png');
+        this.load.image('star', 'assets/squirrel.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        //this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('dude', 'assets/charachter.png', { frameWidth: 405, frameHeight: 457 });
     },
     create: function() {
-        this.plane = this.add.image(640, 360, "plane");
+        this.add.image(400, 300, 'sky');
+        var platforms;
+        platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
+        var player;
+        player = this.physics.add.sprite(100, 450, 'dude');
+        player.setScale(0.2);
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
     },
     update: function() {}
 });
