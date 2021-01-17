95d7c49f85d3bb1cbc6c7161b8d9474f2f459ddc
Owen Valdescruz <42687533+ov37@users.noreply.github.com>
2021-01-17 14:46:52 -0500

    Update scene-one.js

--- scene-one.js
+++ scene-one.js
@@ -4,11 +4,46 @@ var SceneOne = new Phaser.Class({
         Phaser.Scene.call(this, { "key": "SceneOne" });
     },
     init: function() {},
-    preload: function() {},
+    preload: function() {
+        this.load.image('ground', 'assets/platform.png');
+        this.load.spritesheet('dude', 'assets/charachter.png', { frameWidth: 405, frameHeight: 457 });
+    },
     create: function() {
+        
+        platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        
+        player = this.physics.add.sprite(100, 450, 'dude');
+        player.setScale(0.2);
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+        
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 1, end: 4 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 0 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        
+        this.physics.add.collider(player, platforms);
+        
         var text = this.add.text(
-            400, 
-            300, 
+            50, 
+            50, 
             "Meet Owen", 
             {
                 fontSize: 50,
