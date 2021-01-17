24642792ef820b005fada2de43b7b5678a04d29f
Owen Valdescruz <42687533+ov37@users.noreply.github.com>
2021-01-17 11:52:30 -0500

    Update title-scene.js

--- title-scene.js
+++ title-scene.js
@@ -5,17 +5,21 @@ var TitleScene = new Phaser.Class({
     },
     init: function() {},
     preload: function() {
-        this.load.image("plane", "phaser.png");
+        this.load.image("top", "owens.png");
+        this.load.image("middle", "leadership.png");
+        this.load.image("bottom", "portfolio.png");
     },
     create: function() {
-        this.plane = this.add.image(400, 300, "plane");
-        this.time.addEvent({
+        this.top = this.add.image(400, 200, "top");
+        this.middle = this.add.image(400, 300, "middle");
+        this.bottom = this.add.image(400, 400, "bottom");
+        /*this.time.addEvent({
             delay: 3000,
             loop: false,
             callback: () => {
                 this.scene.start("SceneOne");
             }
-        })
+        })*/
     },
     update: function() {}
 });
