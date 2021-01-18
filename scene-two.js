e201ee2b6a841bd366673894f155566a9e02ebcf
Owen Valdescruz <42687533+ov37@users.noreply.github.com>
2021-01-17 23:54:01 -0500

    Update scene-two.js

--- scene-two.js
+++ scene-two.js
@@ -112,16 +112,16 @@ var SceneTwo = new Phaser.Class({
         
         
         this.time.addEvent({
-            delay: 15000,
+            delay: 16000,
             loop: false,
             callback: () => {
                 var stars = this.add.image(400, 300, 'stars');
-                var bbcodetext5 = this.add.rexBBCodeText(50, 100, '', { fontSize: '30px', wrap: { mode: 'word', width: 700 } } );
+                var bbcodetext5 = this.add.rexBBCodeText(20, 50, '', { fontSize: '25px', wrap: { mode: 'word', width: 780 } } );
 
-                bbcodetext5.typing = this.plugins.get('rextexttypingplugin').add(bbcodetext5, typeConfig).start('My first core value is [color=red][b]being present[/b][/color]. I pay close attention to my surroundings and the implicit and explicit emotions of my teamates. I value flexibility and diverse ideas and am always open to changing plans. I think about how to maximize today without minimizing the future.\n\nAnother core value of mine is [color=red][b]helping others[\b][\color]. I want to be a leader people can count on and feel safe around. I desire person-to-person interactions and direct impact rather than a broad or abstract reach. People’s emotional and mental health are more important and have more weight on my decision making than productivity or cost.\n\nMy [color=red][b]purpose[/b][/color] is to provide connection and security to those who feel alone.');
+                bbcodetext5.typing = this.plugins.get('rextexttypingplugin').add(bbcodetext5, typeConfig).start('My first core value is [color=red][b]being present[/b][/color]. I pay close attention to my surroundings and the implicit and explicit emotions of my teamates. I value flexibility and diverse ideas and am always open to changing plans. I think about how to maximize today without minimizing the future.\n\nAnother core value of mine is [color=red][b]helping others[/b][/color]. I want to be a leader people can count on and feel safe around. I desire person-to-person interactions and direct impact rather than a broad or abstract reach. People’s emotional and mental health are more important and have more weight on my decision making than productivity or cost.\n\nMy [color=red][b]purpose[/b][/color] is to provide connection and security to those who feel alone.');
                 
                 this.time.addEvent({
-                    delay: 25000,
+                    delay: 35000,
                     loop: false,
                     callback: () => {
                         stars.visible = false;
